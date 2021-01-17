df3265b422ec9485a6c3332af1f9ac6fb35450f0
Santiago Acosta <santiav@gmail.com>
2021-01-16 22:44:12 -0300

    agregada localstorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Formulario from './components/Formulario'
 import Turno from './components/Turno'
 
 function App() {
 
+  // turnos en local storage
+  let turnosIniciales = JSON.parse(localStorage.getItem('turnos')) // obj pasa a json
+  if (!turnosIniciales) {
+    turnosIniciales = [];
+  }
+
   // Array de turnos
-  const [turnoS, guardarTurnoS] = useState([])
+  const [turnoS, guardarTurnoS] = useState(turnosIniciales)
+
+  // useEffect para realizar operaciones cuando el state cambia (siempre es arrow function) es como "document.ready"
+  useEffect( () => {
+    if (turnosIniciales) {
+      localStorage.setItem('turnos', JSON.stringify(turnoS)) // json pasa a string
+    } else {
+      localStorage.setItem('turnos', JSON.stringify([]))
+    }
+  }, [turnoS]) // el array como 2do parámetro permite que no cicle una api, porque sólo se ejecutará una vez (a la vez, el state dentro, se actualizará 'array de dependencias')
 
   // FUNCIÓN que tome las citas actuales y agregue la nueva
   const crearTurno = turno => {
